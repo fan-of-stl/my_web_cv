d0979572b96f5071f31a7641c339e419bf996c61
bit-2-bytes <ajaiss1713@gmail.com>
2023-07-16 23:25:45 +0530

    ui updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,42 @@
 "use client";
-import { useRouter } from 'next/navigation';
-import styles from './page.module.css'
-import React, { useState } from 'react';
-
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
+import React, { useState } from "react";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 export default function Home() {
   const router = useRouter();
-  
-  function openResumeCreator(){
-    router.push('/CVCreator/');
+
+  function openResumeCreator() {
+    router.push("/CVCreator/");
   }
 
   return (
-    <>
-    <button onClick={openResumeCreator}>Create My Page</button>
-    </>
-  )
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <div className={styles.dots}>
+          <div className={styles.dot1}></div>
+          <div className={styles.dot2}></div>
+          <div className={styles.dot3}></div>
+        </div>
+        <div className={styles.innerContents}>
+          <div className={styles.appContents}>
+        <h1 className={styles.heading}>Portfoilon</h1>
+        <p>
+          A no code Web based portfolio creator for all professionals and
+          graduates. 
+        </p>
+        <button className={styles.button} onClick={openResumeCreator}>Create My Portfolio/CV</button>
+        </div>
+        <Player
+          autoplay
+          loop
+          src="./assets/resume_animated_lottie.json"
+          className={styles.lottiePlayer}
+        >
+        </Player>
+        </div>
+      </div>
+    </div>
+  );
 }
